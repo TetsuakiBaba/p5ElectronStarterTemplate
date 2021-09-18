6e6823aff859f60952dc5aa3a00cfa695fd70414
Tetsuaki Baba <baba@bagpipe.local>
2021-09-18 17:35:35 +0900

    first commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    console.log("hello p5js")
+    var mycanvas = createCanvas(windowWidth, windowHeight);
+    document.getElementById("canvas_placeholder").append(mycanvas.elt);
+}
+
+function draw() {
+    background(0);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text("hello p5js and electron", width / 2, height / 2);
+    circle(mouseX, mouseY, 20);
+}
